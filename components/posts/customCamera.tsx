e532a8583381529954ead2dd53b4b7e987e1abd8
“david <davzy007@gmail.com>
2025-08-29 09:53:15 +0100

    added a good camera component

--- components/posts/customCamera.tsx
+++ components/posts/customCamera.tsx
@@ -1,9 +1,83 @@
 
 import { showSettingsAlert } from "@/utils/helpers";
 import { SafeAreaView, StyleSheet } from "react-native";
-import { Camera, useCameraDevice, useCameraPermission, useMicrophonePermission } from "react-native-vision-camera";
-import CustomLabel from "../CustomLabel";
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Reanimated, { Extrapolation, interpolate, useAnimatedProps, useSharedValue } from 'react-native-reanimated';
+import { Camera, CameraDevice, CameraProps, useCameraDevice, useCameraPermission, useMicrophonePermission } from "react-native-vision-camera";
 import CustomButton from "../buttons/CustomButton";
+import CustomLabel from "../CustomLabel";
+
+Reanimated.addWhitelistedNativeProps({
+  zoom: true,
+})
+const ReanimatedCamera = Reanimated.createAnimatedComponent(Camera)
+
+type camProps = {
+  device: CameraDevice
+}
+function CameraScreen({ device }: camProps) {
+  const zoom = useSharedValue(device.neutralZoom)
+
+  const zoomOffset = useSharedValue(0);
+  const gesture = Gesture.Pan()
+    .onBegin(() => {
+      zoomOffset.value = zoom.value
+    })
+    .onUpdate(event => {
+      const zoomDelta = -event.translationY / 50
+      const z = zoomOffset.value + zoomDelta
+      // const z = zoomOffset.value * event.scale
+      zoom.value = interpolate(
+        z,
+        [1, 10],
+        [device.minZoom, 10],
+        Extrapolation.CLAMP,
+      )
+    })
+
+  const animatedProps = useAnimatedProps<CameraProps>(
+    () => ({ zoom: zoom.value }),
+    [zoom]
+  )
+
+  return (
+    <GestureDetector gesture={gesture}>
+      <ReanimatedCamera
+        enableZoomGesture
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
+        animatedProps={animatedProps}
+      />
+    </GestureDetector>
+    // <View style={{ backgroundColor: "black", flex: 1 }}>
+    //   <Camera enableZoomGesture photo video style={StyleSheet.absoluteFill} device={device} isActive={true} />
+    // </View>
+  )
+}
+
+function NoCameraFoundScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
+      <CustomLabel textAlign="center" labelText="🤔" fontSize={21} />
+      <CustomLabel width={"80%"} labelText="it appears that this device does not have a camera." textAlign="center" />
+    </SafeAreaView>
+  )
+}
+
+type noPermProps = {
+  missingPermissions: string[]
+  requestPerms: () => void
+}
+function NoPermissionScreen({ missingPermissions, requestPerms }: noPermProps) {
+  return (
+    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
+      <CustomLabel textAlign="center" labelText="🔐" fontSize={21} />
+      <CustomLabel width={"80%"} labelText={`Allow ${missingPermissions.join(" and ")} access to start creating.`} textAlign="center" />
+      <CustomButton type="less-vibrant-text" labelText="Grant Permissions" handleClick={requestPerms} />
+    </SafeAreaView>
+  )
+}
 
 export default function CustomCamera() {
   const device = useCameraDevice("back")
@@ -31,22 +105,15 @@ export default function CustomCamera() {
     if (!hasMicPermission) missingPermissions.push("microphone")
 
     return (
-      <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
-        <CustomLabel textAlign="center" labelText="🔐" fontSize={21} />
-        <CustomLabel width={"80%"} labelText={`Allow ${missingPermissions.join(" and ")} access to start creating.`} textAlign="center" />
-        <CustomButton type="less-vibrant-text" labelText="Grant Permissions" handleClick={requestPerms} />
-      </SafeAreaView>
+      <NoPermissionScreen missingPermissions={missingPermissions} requestPerms={requestPerms} />
     )
   }
 
   if (device == null) return (
-    <SafeAreaView style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "black" }}>
-      <CustomLabel textAlign="center" labelText="🤔" fontSize={21} />
-      <CustomLabel width={"80%"} labelText="it appears that this device does not have a camera." textAlign="center" />
-    </SafeAreaView>
+    <NoCameraFoundScreen />
   )
 
   return (
-    <Camera photo video style={StyleSheet.absoluteFill} device={device} isActive={true} />
+    <CameraScreen device={device} />
   )
 }
